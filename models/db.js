2e6b1907c93592b71bcf797db5acdb6693613b28
ramako <wide12@gmail.com>
2015-01-12 22:21:15 +0100

    Base de datos (Faltan cosas...)

---
+++ models/db.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test'); // Indicamos a la bdd donde se conectara, test creara una base de datos test.
+var models = require('./schema.js')(mongoose);
+
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión:'));
+db.once('open', function (callback) {
+    
+var nuevoUsuario = new models.usuarios({ 
+        nombre: 'prueba', 
+        empresa:'IV',
+        estado:'Offline'
+})
+
+ nuevoUsuario.save(function(err, nuevoUsuario) {
+    if (err)
+        return console.error(err);
+    console.dir(nuevoUsuario);
+      });
+    
+    var nuevoAdministrador = new models.administrador({ 
+        nombre: 'pablo', 
+        empresa:'IV',
+        password:'123'})
+    
+    
+     nuevoAdministrador.save(function(err, nuevoAdministrador) {
+    if (err)
+        return console.error(err);
+    console.dir(nuevoAdministrador);
+      });
+    
+    });
+
+
+
