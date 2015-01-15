624fa5ef16d2b395cdd0d3ce985e1fb3c193ec1c
Israel Blancas <iblancasa@gmail.com>
2015-01-15 10:32:00 +0100

    Arreglando fallo en build en Travis

--- app/keys.js
+++ app/keys.js
@@ -20,6 +20,6 @@ else{//Estamos en local
   exports.ip ="localhost";
 
   //Keys de la API para autentificación en Google+
-  exports.CLIENT_ID = "";
-  exports.CLIENT_SECRET = "";
+  exports.CLIENT_ID = "904562443868-lqhes66t36kt1f1amkc1hi0g8s43ul1r.apps.googleusercontent.com";
+  exports.CLIENT_SECRET = "ZbN7vPWVzOxGJb7o88qmG45A";
 }
