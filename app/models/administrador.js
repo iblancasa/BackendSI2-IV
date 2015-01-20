612d0d848df76aaf9d4b0c924729b61858c94917
ramako <wide12@gmail.com>
2015-01-20 10:11:11 +0100

    Modificado schema.js

---
+++ app/models/administrador.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var models = global.models;
+
+//Nombrar administrador
+exports.nombrarAdministrador = function nombrarAdministrador(callback,nombreEmpresa,idUsuario){
+     var queryNombrarAdministrador = global.model.usuarios;
+}
+
+
+//Obtener conversaciones abiertas al administrador
+exports.conversacionesAdministrador = function conversacionesAdministrador(callback,nombreEmpresa,idUsuario){
+     var queryNombrarAdministrador = global.model.usuarios;
+     
+}
