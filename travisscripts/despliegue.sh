fe04c45b73ce0bdc355e4e2f657249906160741f
Israel Blancas <iblancasa@gmail.com>
2015-01-31 00:44:33 +0100

    Añadiendo creacion de Ansible_hosts #84

--- travisscripts/despliegue.sh
+++ travisscripts/despliegue.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
 ssh-keyscan backendsi2.cloudapp.net >> ~/.ssh/known_hosts;
+echo "[azure] backendsi2.cloudapp.net ansible_connection=ssh ansible_ssh_user=backendsi2 ansible_ssh_pass=$ANSIBLESSH"> ./travisscripts/ansible_hosts;
+export ANSIBLE_HOSTS=./travisscripts/ansible_hosts;
+ansible-playbook despliegue.yml;
 cat ./travisscripts/ok.txt;
