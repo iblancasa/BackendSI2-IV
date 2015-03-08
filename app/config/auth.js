815e9b077bdc10941d3e8c751fa91c13b811a051
Israel Blancas <iblancasa@gmail.com>
2015-03-08 17:14:46 +0100

    Creando primera integración continua tras la reforma

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,4 +1,10 @@
 // config/auth.js
+var host       = process.env.OPENSHIFT_NODEJS_IP || process.env.IP;
+var port     = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+var service = 'http://'+host+':'+port;
+var callFacebook = service+'/auth/facebook/callback';
+var callTwitter = service+'/auth/twitter/callback';
+var callGoogle = service+'/auth/google/callback';
 
 // expose our config directly to our application using module.exports
 module.exports = {
@@ -6,19 +12,19 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: process.env.facebookclientID, // your App ID
 		'clientSecret' 	:  process.env.facebookclientSecret, // your App Secret
-		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+		'callbackURL' 	: callFacebook
 	},
 
 	'twitterAuth' : {
 		'consumerKey' 		:  process.env.twitterconsumerKey,
 		'consumerSecret' 	:  process.env.twitterconsumerSecret,
-		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+		'callbackURL' 		: callTwitter
 	},
 
 	'googleAuth' : {
 		'clientID' 		:  process.env.googleclientID,
 		'clientSecret' 	:  process.env.googleclientSecret,
-		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+		'callbackURL' 	: callGoogle
 	}
 
 };
